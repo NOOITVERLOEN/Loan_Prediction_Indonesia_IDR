f96e3bff182504a5a7907dc1541992f2f953805f
NOE <114326587+NOOITVERLOEN@users.noreply.github.com>
2026-07-14 23:45:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ with col_r:
     tenor_pinjaman = st.selectbox(
         "Tenor Pinjaman", TENOR_OPTIONS,
         index=TENOR_OPTIONS.index(360),
-        format_func=lambda x: f"{x} bulan  (~{x // 12} tahun)",
+        format_func=lambda x: f"{x} Hari  (~{x // 12} Bulan)",
     )
     riwayat_kredit = st.selectbox("Riwayat Kredit", CATEGORY_OPTIONS["Riwayat_Kredit"],
                                    help="Riwayat pembayaran kredit sebelumnya")
